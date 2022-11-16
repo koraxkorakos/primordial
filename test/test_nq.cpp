b8b07234c1b8fb5682a223af83e13d483f9076ea
Christian Kotz <christian.kotz@itk-engineering.com>
2022-11-16 09:38:41 +0100

    more tests

--- test/test_nq.cpp
+++ test/test_nq.cpp
@@ -1,5 +1,6 @@
 
 #include <primordial/nq.hpp>
+#include <primordial/nq_io.hpp>
 //#include <primordial/details/mult128.hpp>
 #include <gtest/gtest.h>
 #include <iterator>
@@ -57,4 +58,42 @@ TEST(NqTests,  test_NQ_bool_conversion)
    EXPECT_TRUE(NQ(2));
    EXPECT_TRUE(NQ(1,3));
    EXPECT_TRUE(NQ(2,3));
+}
+
+TEST(NqTests,  test_NQ_euqal)
+{
+   EXPECT_TRUE(NQ(2,3) == NQ(2,3));
+   EXPECT_FALSE(NQ(2,3) == NQ(3,1));
+   EXPECT_FALSE(NQ(2,3) == NQ(2,1));
+   EXPECT_FALSE(NQ(2,3) == NQ(1,3));
+   EXPECT_TRUE(NQ::zero() == NQ::zero());
+   EXPECT_TRUE(NQ::defective() == NQ::defective());
+   EXPECT_FALSE(NQ::defective() == NQ::zero());
+   EXPECT_FALSE(NQ::defective() == NQ(2,3));
+   EXPECT_FALSE(NQ(2,3) == NQ::defective());  
+   EXPECT_FALSE(NQ::zero() == NQ::defective());
+   EXPECT_FALSE(NQ::zero() == NQ(2,3));
+   EXPECT_FALSE(NQ(2,3) == NQ::zero());
+}
+
+TEST(NqTests,  test_NQ_uneuqal)
+{
+   EXPECT_FALSE(NQ(2,3) != NQ(2,3));
+   EXPECT_TRUE(NQ(2,3) != NQ(3,1));
+   EXPECT_TRUE(NQ(2,3) != NQ(2,1));
+   EXPECT_TRUE(NQ(2,3) != NQ(1,3));
+   EXPECT_FALSE(NQ::zero() != NQ::zero());
+   EXPECT_FALSE(NQ::defective() != NQ::defective());
+   EXPECT_TRUE(NQ::defective() != NQ::zero());
+   EXPECT_TRUE(NQ::defective() != NQ(2,3));
+   EXPECT_TRUE(NQ(2,3) != NQ::defective());  
+   EXPECT_TRUE(NQ::zero() != NQ::defective());
+   EXPECT_TRUE(NQ::zero() != NQ(2,3));
+   EXPECT_TRUE(NQ(2,3) != NQ::zero());   
+}
+
+
+TEST(NqTests,  test_NQ_uminus)
+{
+   EXPECT_EQ(-NQ(2,3), NQ(3,2));
 }
